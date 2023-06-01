d0021b128f5528c3dbce3584b345336ddbe3cd94
chiliphrosting <pransbottom@yahoo.com>
2023-06-01 17:41:56 -0400

    1Jun23: rm js/avrs-stick-header.js from .gitignore sets inline style attribute

---
+++ js/avrs-sticky-header.js
@@ -0,0 +1,21 @@
+let header = document.getElementById("fh5co-header");
+
+header.setAttribute("style", "position: fixed"); 
+
+/**
+window.onscroll = function() {stickyFun()};
+
+var header = document.getElementById("fh5co-header");
+var sticky = header.offsetTop;
+
+function stickyFun(){
+	if (window.pageYOffset > sticky) {
+		header.classList.add("sticky");
+	} else {
+		header.classList.remove("sticky")
+	}
+}
+
+
+**/
+console.log("sticky-header-js active")
