9fc6d7f755131808ae961f6c4ed2c141fb7bcb31
chiliphrosting <pransbottom@yahoo.com>
2023-06-02 13:55:26 -0400

    2Jun23: tested lazy load, data-src attribute does not load high-res img; added avrs-lazy-style.js active not usable; minor revs

---
+++ js/avrs-lazy-style.js
@@ -0,0 +1,12 @@
+let indexImgLazy = document.getElementById("index-intro-img");
+/**
+setTimeout(indexImgLoader, 5000)
+
+function indexImgLoader() {
+indexImgLazy.setAttribute("style", "background-image: url(https://docs.google.com/drawings/d/e/2PACX-1vS5p96POzPPFk_YXceeI7qbm-qWP2Q9tmEnKZptKMrqI1zxD28SdyRMMbiVyXaURW60QSM5vCrxsVPP/pub?w=3024&h=2016); width: 100%; float: left; margin-right: -100%; position: relative; opacity: 0; display: block: z-index: 1; height: 979px;"); 
+
+};
+
+**/
+
+console.log("js-lazy-style active")
