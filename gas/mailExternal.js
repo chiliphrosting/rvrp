316d3e613faf150486fc4a63c7227746e75fd66a
chiliphrosting <pransbottom@yahoo.com>
2026-02-10 15:16:19 -0500

    gz6: fixed mailExternal.js column variables not matching with label in email

--- gas/mailExternal.js
+++ gas/mailExternal.js
@@ -1,8 +1,7 @@
 /****************
 this is only active in Google App Scripts!!!
 ****************/
-
-
+// 2026v1.1E.1_live_10Hxq2EYN26
 // GLOBAL VARIABLES within project!!!
 // The URL of the spreadsheet
 const THISSHTURL = "copy-and-paste-the-spreadsheet-url-on-the-active-sheet";
@@ -10,9 +9,7 @@ const SS = SpreadsheetApp.openByUrl(THISSHTURL);
 // unused sheet id
 const SHTID = SS.getSheetId().toString();
 
-
 function infoGetter() {
-
   let lr = SpreadsheetApp.openByUrl(THISSHTURL).getLastRow();
   let lc = SpreadsheetApp.openByUrl(THISSHTURL).getLastColumn();
 
@@ -22,35 +19,39 @@ function infoGetter() {
     .getValues();
   let valsAll = fullRow[0];
 
-  var allText = `DATE: ${valsAll[0]}\n\nNAME: ${valsAll[1]} ${valsAll[3]}\nEmail:  ${valsAll[2]}\nPhone Number: ${valsAll[6]}\n\nWhat would you like to inquire about?: ${valsAll[4]}\n\nInfo about project: ${valsAll[5]}\n`;
-
+  var allText = `DATE: ${valsAll[0]}\n\nNAME: ${valsAll[2]} ${
+    valsAll[3]
+  }\nEmail:  ${valsAll[1]}\nPhone Number: ${
+    valsAll[4]
+  }\n\nWhat would you like to inquire about?: ${
+    valsAll[5]
+  }\n\nInfo about project: ${valsAll[6]}\n`;
   return allText;
-
-};
+}
 
 function infoEmailer(subject, body) {
-
   let emailRecepient = "your-email@some-domain.com";
   MailApp.sendEmail(emailRecepient, subject, body);
-};
+}
 
 //DO NOT USE 26May23
 function infoSummarySendToOwner() {
-  var todayDate = new Date ();
+  var todayDate = new Date();
   var subjectText = `RVRP website form: ${todayDate}`;
   var infoSetter = infoGetter();
-  infoEmailer(subjectText, infoSetter)
-
+  infoEmailer(subjectText, infoSetter);
 }
 
 //USE 26May23
 function emailSendToOwner() {
-
-GmailApp.sendEmail("reliablevirtualreal@yahoo.com", `RVRP website form: ${ new Date()}`, infoGetter() )
-
+  GmailApp.sendEmail(
+    "reliablevirtualreal@yahoo.com",
+    `RVRP website form: ${new Date()}`,
+    infoGetter(),
+  );
 }
 
 /****** ONLY TESTS VALUES THAT ARE PASSED ******/
 function tester() {
-  Logger.log(`${infoGetter()} and ${todayDate}`)
+  Logger.log(`${infoGetter()} and ${todayDate}`);
 }
